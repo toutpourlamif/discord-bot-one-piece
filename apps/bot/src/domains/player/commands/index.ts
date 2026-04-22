b5be690b42d0fed23f4ef89f9d7a42dc9ce3b56e
402rayan <rayan.desplanque-hamadache@lacatholille.fr>
2026-04-22 23:05:14 +0200

    remove useless type

--- apps/bot/src/domains/player/commands/index.ts
+++ apps/bot/src/domains/player/commands/index.ts
@@ -1,6 +1,4 @@
-import type { Command } from '../../../shared/command.js';
-
 import { debugCommand } from './debug.js';
 import { karmaCommand } from './karma.js';
 
-export const playerCommands: Array<Command> = [karmaCommand, debugCommand];
+export const playerCommands = [karmaCommand, debugCommand];
