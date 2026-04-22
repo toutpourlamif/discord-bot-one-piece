a4caac0b29fec59f25883abcb6632c7bb91d1392
Ilyess Oughaddou <oughaddou.ilyess@gmail.com>
2026-04-22 20:38:34 +0200

    url handler update + lengh max name

--- packages/db/src/domains/resource/resource_template/schema.ts
+++ packages/db/src/domains/resource/resource_template/schema.ts
@@ -1,11 +1,11 @@
 import { pgTable, text, varchar, serial } from 'drizzle-orm/pg-core';
 
-import { timestamps } from '../../../shared/helpers.js';
+import { imageUrl, timestamps } from '../../../shared/helpers.js';
 
 export const resourceTemplate = pgTable('resource_template', {
   id: serial('id').primaryKey(),
-  name: varchar('name').notNull(),
-  imageUrl: text('image_url'),
+  name: varchar('name', { length: 128 }).notNull(),
+  ...imageUrl(),
   description: text('description'),
   ...timestamps(),
 });
