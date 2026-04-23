d5355809c3a81e35d2906ec4c13f3e9b053d0f0c
Hakim Madani <107553328+Jhizzaiolo@users.noreply.github.com>
2026-04-24 00:21:12 +0200

    added new db:studio version (#79)
    * added new db:studio version
    
    * added advices by rayan
    
    * change timeout to 0.5s + change location of file to scripts
    
    ---------
    
    Co-authored-by: 402rayan <rayan.desplanque-hamadache@lacatholille.fr>

---
+++ packages/db/scripts/studio.ts
@@ -0,0 +1,12 @@
+import { spawn } from 'child_process';
+
+import open from 'open';
+
+// 1. Lance drizzle-kit studio en arrière-plan
+const child = spawn('drizzle-kit', ['studio'], { stdio: 'inherit' });
+process.on('SIGINT', () => child.kill('SIGINT'));
+
+// 2. Attend 2s puis ouvre le navigateur
+setTimeout(() => {
+  void open('https://local.drizzle.studio');
+}, 500);
