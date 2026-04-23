6b7ec1502b6a1a3a0d3553760c8ee2f74ecd4d80
402rayan <rayan.desplanque-hamadache@lacatholille.fr>
2026-04-23 21:53:11 +0200

    export ResourceName type + fix seed

--- packages/db/src/domains/resource/index.ts
+++ packages/db/src/domains/resource/index.ts
@@ -1,2 +1,3 @@
+export type { ResourceName } from './resource_template/data.js';
 export { seedResources } from './resource_template/seed.js';
 export { resourceTemplate, type ResourceTemplate, type ResourceTemplateInsert } from './resource_template/schema.js';
