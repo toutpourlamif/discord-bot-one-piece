556772cf2f39654502828dfcfccb78cd1d7a3a1d
Ilyess Oughaddou <oughaddou.ilyess@gmail.com>
2026-04-23 12:35:58 +0200

    feat(dev): add nombre button command

--- apps/bot/src/domains/_dev/commands/index.ts
+++ apps/bot/src/domains/_dev/commands/index.ts
@@ -1,7 +1,8 @@
 import { colorCommand } from './color.js';
 import { embedCommand } from './embed.js';
 import { moiCommand } from './moi.js';
+import { nombreCommand } from './nombre.js';
 import { onePieceCommand } from './onepiece.js';
 import { repeatCommand } from './repeat.js';
 
-export const devCommands = [onePieceCommand, repeatCommand, embedCommand, moiCommand, colorCommand];
+export const devCommands = [onePieceCommand, repeatCommand, embedCommand, moiCommand, colorCommand, nombreCommand];
