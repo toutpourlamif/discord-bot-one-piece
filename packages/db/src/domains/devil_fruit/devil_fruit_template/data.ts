05ea8d0c142131c02760bfbc97e25a32ddc0d299
mathisnnomo <mathis.nnomo@epitech.eu>
2026-04-23 18:37:12 +0200

    ADD Ajout des description pour fruit en DB

--- packages/db/src/domains/devil_fruit/devil_fruit_template/data.ts
+++ packages/db/src/domains/devil_fruit/devil_fruit_template/data.ts
@@ -7,6 +7,8 @@ export const DEVIL_FRUIT_TEMPLATES_DATA: Array<DevilFruitTemplateInsert> = [
     hpBonus: 0,
     combatBonus: 20,
     imageUrl: 'devil-fruits/gomu-gomu-no-mi.webp',
+    description:
+      "Le Gomu Gomu no Mi est un fruit du démon de type Paramecia qui confère à son utilisateur la capacité de transformer son corps en caoutchouc. Cela lui permet d'étirer, de gonfler et de rebondir de manière élastique, offrant une grande résistance aux attaques physiques et la capacité d'effectuer des mouvements uniques en combat.",
   },
   {
     name: 'Mera Mera no Mi',
@@ -14,6 +16,8 @@ export const DEVIL_FRUIT_TEMPLATES_DATA: Array<DevilFruitTemplateInsert> = [
     hpBonus: 0,
     combatBonus: 25,
     imageUrl: 'devil-fruits/mera-mera-no-mi.webp',
+    description:
+      "Le Mera Mera no Mi est un fruit du démon de type Logia qui confère à son utilisateur la capacité de créer et de contrôler le feu. Cela lui permet d'attacker avec une grande puissance et de se défendre contre les ennemis.",
   },
   {
     name: 'Hie Hie no Mi',
--- packages/db/src/domains/devil_fruit/devil_fruit_template/schema.ts
+++ packages/db/src/domains/devil_fruit/devil_fruit_template/schema.ts
@@ -1,5 +1,5 @@
 import { sql } from 'drizzle-orm';
-import { index, integer, pgTable, serial, varchar } from 'drizzle-orm/pg-core';
+import { index, integer, pgTable, serial, text, varchar } from 'drizzle-orm/pg-core';
 
 import { imageUrl, timestamps } from '../../../shared/helpers.js';
 import { devilFruitType } from '../enum.js';
@@ -16,6 +16,8 @@ export const devilFruitTemplate = pgTable(
     hpBonus: integer('hp_bonus').notNull().default(0),
     combatBonus: integer('combat_bonus').notNull().default(0),
     ...imageUrl(),
+    ...timestamps(),
+    description: text('description'),
     ...timestamps(),
   },
   (table) => [index('devil_fruit_template_name_trgm_idx').using('gin', sql`${table.name} gin_trgm_ops`)],
