fbc30b91fc20d748d2b3e5e55e4e2373cae30f63
Ilyess Oughaddou <oughaddou.ilyess@gmail.com>
2026-04-24 21:10:11 +0200

    fix(db): regenerate resource instance migration

--- packages/db/drizzle/0011_gigantic_adam_warlock.sql
+++ packages/db/drizzle/0011_gigantic_adam_warlock.sql
@@ -8,7 +8,6 @@ CREATE TABLE "resource_instance" (
 	CONSTRAINT "resource_instance_quantity_non_negative" CHECK ("resource_instance"."quantity" >= 0)
 );
 --> statement-breakpoint
-DROP TABLE "character_template" CASCADE;--> statement-breakpoint
 ALTER TABLE "resource_instance" ADD CONSTRAINT "resource_instance_template_id_resource_template_id_fk" FOREIGN KEY ("template_id") REFERENCES "public"."resource_template"("id") ON DELETE restrict ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "resource_instance" ADD CONSTRAINT "resource_instance_player_id_player_id_fk" FOREIGN KEY ("player_id") REFERENCES "public"."player"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 CREATE UNIQUE INDEX "resource_instance_player_template_uniq" ON "resource_instance" USING btree ("player_id","template_id");
