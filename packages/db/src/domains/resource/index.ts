5c8ce24891620102070accded5df1030f2df8b0e
Ilyess Oughaddou <oughaddou.ilyess@gmail.com>
2026-04-24 16:46:16 +0200

    feat(db): add resource instance table

--- packages/db/src/domains/resource/index.ts
+++ packages/db/src/domains/resource/index.ts
@@ -1,2 +1,3 @@
+export { resourceInstance } from './resource_instance/schema.js';
 export { seedResources } from './resource_template/seed.js';
 export { resourceTemplate, type ResourceTemplate, type ResourceTemplateInsert } from './resource_template/schema.js';
