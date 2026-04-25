da448baf40a4b3e78e548c51dc7fcbd0c5e4eb54
Hakim <hakim.madani06@gmail.com>
2026-04-25 19:23:05 +0200

    modified for buildOpEmbed

--- apps/bot/src/domains/_dev/commands/emoji.ts
+++ apps/bot/src/domains/_dev/commands/emoji.ts
@@ -1,15 +1,15 @@
 import { ActionRowBuilder, ButtonBuilder, ButtonStyle } from 'discord.js';
 import sampleSize from 'lodash/sampleSize.js';
 
-import { createOpEmbed } from '../../../discord/embed/create-op-embed.js';
 import type { Command } from '../../../discord/types.js';
+import { buildOpEmbed } from '../../../discord/utils/build-op-embed.js';
 
 const EMOJIS: Array<string> = ['🍖', '🏴‍☠️', '⚓', '🐉', '🗺️', '🛶', '☠️', '🍈', '🦈', '🐟', '🏝️', '⚔️', '🔫', '🔪'];
 
 export const emojiCommand: Command = {
   name: 'emoji',
   async handler(message, args) {
-    const embed = createOpEmbed().setTitle('Emojis').setDescription('Choisis un emoji ! ');
+    const embed = buildOpEmbed().setTitle('Emojis').setDescription('Choisis un emoji ! ');
     const fiveEmojis = sampleSize(EMOJIS, 5);
     const buttons = fiveEmojis.map((emojis, index) =>
       new ButtonBuilder().setCustomId(`emoji:${index}`).setEmoji(emojis).setStyle(ButtonStyle.Secondary),
