2369358fd284355a5bdef32075271a6c184a9c57
Hakim <hakim.madani06@gmail.com>
2026-04-26 10:56:20 +0200

    added characters into db

--- packages/db/src/seed.ts
+++ packages/db/src/seed.ts
@@ -1,10 +1,12 @@
 import { closeDb, db } from './client.js';
+import { seedCharacter } from './domains/character/character_template/seed.js';
 import { seedDevilFruits } from './domains/devil_fruit/index.js';
 import { seedResources } from './domains/resource/index.js';
 
 try {
   await seedDevilFruits(db);
   await seedResources(db);
+  await seedCharacter(db);
   console.log('Seed terminé');
 } catch (err) {
   console.error('Seed failed:', err);
