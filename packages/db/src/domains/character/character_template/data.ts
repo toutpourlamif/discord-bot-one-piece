0683b77cc2f39ebcffef42676b344fee0695657e
Hakim <hakim.madani06@gmail.com>
2026-04-26 13:56:11 +0200

    added recommended changes

--- packages/db/src/domains/character/character_template/data.ts
+++ packages/db/src/domains/character/character_template/data.ts
@@ -1,24 +1,22 @@
 import type { CharacterTemplateInsert } from './schema.js';
-
+// TODO: supprimer/modifier en prod
 export const CHARACTER_TEMPLATES_DATA: Array<CharacterTemplateInsert> = [
   {
     name: 'Monkey D. Luffy',
     hp: 10,
     combat: 10,
-    imageUrl:
-      'https://static.wikia.nocookie.net/onepiece/images/7/7f/Monkey_D._Luffy_Anime_Post_Ellipse_Infobox.png/revision/latest?cb=20170617172443&path-prefix=fr',
+    imageUrl: '',
   },
   {
     name: 'Roronoa Zoro',
     hp: 10,
     combat: 10,
-    imageUrl: 'https://www.tabloide.fr/wp-content/uploads/2021/11/Tableau-Roronoa-Zoro-one-piece.jpg',
+    imageUrl: '',
   },
   {
     name: 'Shanks',
     hp: 10,
     combat: 10,
-    imageUrl:
-      'https://static.wikia.nocookie.net/onepiece/images/6/66/Shanks_Anime_Infobox.png/revision/latest?cb=20240829193446&path-prefix=fr',
+    imageUrl: '',
   },
 ];
