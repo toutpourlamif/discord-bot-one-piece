7f172e47df7d4e8f4d9c8e3e076fd7785e2a1d57
Jhizzaiolo <hakim.madani06@gmail.com>
2026-04-27 18:36:32 +0200

    re added changes

--- packages/db/src/domains/character/character_template/data.ts
+++ packages/db/src/domains/character/character_template/data.ts
@@ -5,18 +5,18 @@ export const CHARACTER_TEMPLATES_DATA: Array<CharacterTemplateInsert> = [
     name: 'Monkey D. Luffy',
     hp: 10,
     combat: 10,
-    imageUrl: 'null',
+    imageUrl: null,
   },
   {
     name: 'Roronoa Zoro',
     hp: 10,
     combat: 10,
-    imageUrl: 'null',
+    imageUrl: null,
   },
   {
     name: 'Shanks',
     hp: 10,
     combat: 10,
-    imageUrl: 'null',
+    imageUrl: null,
   },
 ];
