5534f531a417a3013614fd9d7774e1f1a71c0ab9
mathisnnomo <mathis.nnomo@epitech.eu>
2026-04-27 16:47:36 +0200

    FIX mettre tout dans un même fichier v2

--- apps/bot/src/domains/_dev/commands/randomcat-button.ts
+++
@@ -1,5 +0,0 @@
-import { ButtonBuilder, ButtonStyle } from 'discord.js';
-
-export function buildRandomCatButton(): ButtonBuilder {
-  return new ButtonBuilder().setCustomId('randomcat').setLabel('Chat').setEmoji('🐱').setStyle(ButtonStyle.Secondary).setDisabled(true);
-}
