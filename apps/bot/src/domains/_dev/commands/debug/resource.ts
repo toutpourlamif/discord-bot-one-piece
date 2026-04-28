b744213dfc01d340e0e231592ea045f7ca421a60
ilyees91 <93188666+ilyees91@users.noreply.github.com>
2026-04-28 10:42:51 +0200

    feat: add polyvalent debug command (#110)
    * feat: add polyvalent debug command
    
    * refactor(bot): split debug command handlers
    
    * refactor: simplify debug subcommand handlers

---
+++ apps/bot/src/domains/_dev/commands/debug/resource.ts
@@ -0,0 +1,18 @@
+import { NotFoundError } from '../../../../discord/errors.js';
+import type { Command } from '../../../../discord/types.js';
+import { parseIntegerArg } from '../../../../discord/utils/parse-integer-arg.js';
+import * as resourceRepository from '../../../resource/repository.js';
+
+import { replyDebugData } from './utils.js';
+
+export const handleResource: Command['handler'] = async (message, args) => {
+  const id = parseIntegerArg(args[0]);
+
+  const resource = await resourceRepository.findById(id);
+
+  if (!resource) {
+    throw new NotFoundError('Resource introuvable.');
+  }
+
+  await replyDebugData(message, resource);
+};
