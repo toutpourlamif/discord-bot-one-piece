6f4aef0602ce2a4732487d7a68511d21bd5fcd2f
402rayan <rayan.desplanque-hamadache@lacatholille.fr>
2026-04-29 18:13:46 +0200

    ADD DEVIL FRUITS TO CHARACTERS

--- packages/db/src/domains/character/character_template/schema.ts
+++ packages/db/src/domains/character/character_template/schema.ts
@@ -2,6 +2,7 @@ import { sql } from 'drizzle-orm';
 import { index, integer, pgTable, serial, varchar } from 'drizzle-orm/pg-core';
 
 import { imageUrl, timestamps } from '../../../shared/helpers.js';
+import { devilFruitTemplate } from '../../devil_fruit/devil_fruit_template/schema.js';
 
 export const characterTemplate = pgTable(
   'character_template',
@@ -10,6 +11,9 @@ export const characterTemplate = pgTable(
     name: varchar('name', { length: 128 }).notNull().unique(),
     hp: integer('hp').notNull().default(100),
     combat: integer('combat').notNull().default(10),
+    devilFruitTemplateId: integer('devil_fruit_template_id').references(() => devilFruitTemplate.id, {
+      onDelete: 'restrict',
+    }),
     ...imageUrl(),
     ...timestamps(),
   },
