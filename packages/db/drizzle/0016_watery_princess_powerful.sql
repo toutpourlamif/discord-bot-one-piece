58811d094dc41736cc11eaf8db4d41a50bc45e86
mathisnnomo <mathis.nnomo@epitech.eu>
2026-04-29 18:30:26 +0200

    DELETE redundant 16 migration

--- packages/db/drizzle/0016_watery_princess_powerful.sql
+++
@@ -1,4 +0,0 @@
-CREATE TYPE "public"."rarity" AS ENUM('COMMON', 'RARE', 'VERY_RARE', 'LEGENDARY');--> statement-breakpoint
-ALTER TABLE "character_template" ADD COLUMN "rarity" "rarity" DEFAULT 'COMMON' NOT NULL;--> statement-breakpoint
-ALTER TABLE "devil_fruit_template" ADD COLUMN "rarity" "rarity" DEFAULT 'COMMON' NOT NULL;--> statement-breakpoint
-ALTER TABLE "resource_template" ADD COLUMN "rarity" "rarity" DEFAULT 'COMMON' NOT NULL;
