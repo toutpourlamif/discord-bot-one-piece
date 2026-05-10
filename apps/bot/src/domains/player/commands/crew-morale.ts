cb431d0ddd079f4a828b7c934c787be603147e6b
Jhizzaiolo <hakim.madani06@gmail.com>
2026-05-10 14:14:21 +0200

    added to index

--- apps/bot/src/domains/player/commands/crew-morale.ts
+++ apps/bot/src/domains/player/commands/crew-morale.ts
@@ -3,7 +3,7 @@ import { getCrewMoraleGrade } from '../../crew/crewMorale.js';
 
 // TODO: supprimer avant la PROD - commande debug crew morale
 export const crewMoraleCommand: Command = {
-  name: 'crew-morale',
+  name: ['crew-morale', 'crewmorale'],
   async handler({ message, player }) {
     const grade = getCrewMoraleGrade(player.crewMorale);
     await message.reply(`Morale d'équipage: ${grade} (${player.crewMorale})`);
