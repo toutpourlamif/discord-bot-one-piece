addf6d1571cb851d972a14aa2a7f1c79d01fa940
Jhizzaiolo <hakim.madani06@gmail.com>
2026-05-15 12:41:32 +0200

    added create

--- packages/db/drizzle/0036_gluant.sql
+++ packages/db/drizzle/0036_gluant.sql
@@ -1,1 +1,2 @@
+CREATE TYPE "character_skill" AS ENUM('MASON', 'SUPER_MASON', 'NAVIGATOR', 'CONQUERORS_HAKI', 'ARCHAEOLOGIST');
 ALTER TABLE "character_template" ADD COLUMN "skill" character_skill[];
