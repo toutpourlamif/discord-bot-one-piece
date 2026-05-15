6e33307fc7e812f9f38715ab8c4eb6faf02310ff
Jhizzaiolo <hakim.madani06@gmail.com>
2026-05-15 13:06:13 +0200

    added constants

--- apps/bot/src/domains/guild/constants.ts
+++ apps/bot/src/domains/guild/constants.ts
@@ -1,2 +1,4 @@
 export const CONFIRM_SET_PREFIX_BUTTON_NAME = 'setprefixconfirm';
 export const CANCEL_SET_PREFIX_BUTTON_NAME = 'setprefixcancel';
+export const SUPPORTED_LANGUAGES = ['fr', 'en'] as const;
+export type SupportedLanguage = (typeof SUPPORTED_LANGUAGES)[number];
