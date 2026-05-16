cffba88c271a19f9d7cf028d01490a6232c42421
Jhizzaiolo <hakim.madani06@gmail.com>
2026-05-16 14:33:41 +0200

    addes setlanguage command and const

--- apps/bot/src/domains/guild/constants.ts
+++ apps/bot/src/domains/guild/constants.ts
@@ -1,4 +1,6 @@
 export const CONFIRM_SET_PREFIX_BUTTON_NAME = 'setprefixconfirm';
 export const CANCEL_SET_PREFIX_BUTTON_NAME = 'setprefixcancel';
 export const SUPPORTED_LANGUAGES = ['fr', 'en'] as const;
+export const CONFIRM_SET_LANGUAGE_BUTTON_NAME = 'setlanguageconfirm';
+export const CANCEL_SET_LANGUAGE_BUTTON_NAME = 'setlanguagecancel';
 export type SupportedLanguage = (typeof SUPPORTED_LANGUAGES)[number];
