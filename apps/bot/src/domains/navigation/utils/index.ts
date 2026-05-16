b02f316c3b9b8aed0f69270c81bd73fbfd8b90c1
402rayan <79017298+402rayan@users.noreply.github.com>
2026-05-16 18:49:31 +0200

    add compute travel eta (#328)
    * add compute travel eta
    
    * add todos

--- apps/bot/src/domains/navigation/utils/index.ts
+++ apps/bot/src/domains/navigation/utils/index.ts
@@ -1,5 +1,6 @@
 export { isSea } from './is-sea.js';
 export { computeDriftProbability } from './compute-drift-probability.js';
+export { computeTravelETA } from './compute-travel-eta.js';
 export { findEdge } from './find-edge.js';
 export { findPossibleEdges } from './find-possible-edges.js';
 export { hasLogOrEternalPoseForIsland } from './has-log-or-eternal-pose-for-island.js';
