75a85e6630eacbbb303facab3c9c349b78ab9002
402rayan <rayan.desplanque-hamadache@lacatholille.fr>
2026-05-20 22:56:39 +0200

    refactor debug

--- apps/bot/src/domains/_dev/commands/debug/player.ts
+++
@@ -1,9 +0,0 @@
-import type { Command } from '../../../../discord/types.js';
-import { replyDebugData } from '../../../../discord/utils/index.js';
-import { resolveTargetPlayer } from '../../../player/index.js';
-
-export const handlePlayer: Command['handler'] = async (ctx) => {
-  const { targetPlayer } = await resolveTargetPlayer(ctx);
-
-  await replyDebugData(ctx.message, targetPlayer);
-};
