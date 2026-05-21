086b98f94643d4a7e54931d8368c0319721669c4
Hakim Madani <107553328+Jhizzaiolo@users.noreply.github.com>
2026-05-21 17:58:25 +0200

    295 feature skills sur les character templates (#302)
    * added skill in schema and enum
    
    * added some skills
    
    * ADDED requested changes
    
    * added migration
    
    * added create
    
    * added migration
    
    * added migration and export to schema
    
    * delete migration 37 et restore journal
    
    * added migration
    
    * added requested changes
    
    * added requested change
    
    * added new migration
    
    * added migration
    
    * remove 0039
    
    * add migration 41
    
    ---------
    
    Co-authored-by: 402rayan <rayan.desplanque-hamadache@lacatholille.fr>

--- packages/db/src/domains/character/character_template/schema.ts
+++ packages/db/src/domains/character/character_template/schema.ts
@@ -5,6 +5,7 @@ import { imageUrl, timestamps } from '../../../shared/helpers.js';
 import { rarity } from '../../../shared/rarity.js';
 import { devilFruitTemplate } from '../../devil_fruit/devil_fruit_template/schema.js';
 import { characterRaceEnum } from '../enum.js';
+import { characterSkillEnum } from '../skill-enum.js';
 export const characterTemplate = pgTable(
   'character_template',
   {
@@ -26,6 +27,11 @@ export const characterTemplate = pgTable(
     race: characterRaceEnum('race').notNull(),
     ...imageUrl(),
     ...timestamps(),
+
+    skills: characterSkillEnum('skills')
+      .array()
+      .notNull()
+      .default(sql`'{}'::character_skill[]`),
   },
   (table) => [index('character_template_name_trgm_idx').using('gin', sql`${table.name} gin_trgm_ops`)],
 );
