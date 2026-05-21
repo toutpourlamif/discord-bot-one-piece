d576b1eb760b83c3b8cc4eeda6661fec6255919d
Jhizzaiolo <hakim.madani06@gmail.com>
2026-05-21 13:12:39 +0200

    removed migration

--- packages/db/drizzle/0040_ajout_captain_multiplier.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "character_template" ADD COLUMN "captain_combat_multiplier" real DEFAULT 1 NOT NULL;--> statement-breakpoint
-ALTER TABLE "character_template" ADD COLUMN "captain_hp_multiplier" real DEFAULT 1 NOT NULL;--> statement-breakpoint
-ALTER TABLE "character_template" ADD COLUMN "captain_berry_gain_multiplier" real DEFAULT 1 NOT NULL;
