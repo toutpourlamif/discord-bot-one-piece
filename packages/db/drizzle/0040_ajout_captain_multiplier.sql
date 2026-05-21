eaaf34f345fc46ef7d982b64cae9373dee3d1e01
Hakim Madani <107553328+Jhizzaiolo@users.noreply.github.com>
2026-05-21 15:59:08 +0200

    Ajout des multiplicateurs de capitaine (bonus) (#349)
    * added captain multiplier
    
    * removed migration
    
    * added migration

---
+++ packages/db/drizzle/0040_ajout_captain_multiplier.sql
@@ -0,0 +1,5 @@
+ALTER TABLE "character_template" ADD COLUMN "captain_combat_multiplier" real DEFAULT 1 NOT NULL;--> statement-breakpoint
+ALTER TABLE "character_template" ADD COLUMN "captain_hp_multiplier" real DEFAULT 1 NOT NULL;--> statement-breakpoint
+ALTER TABLE "character_template" ADD COLUMN "captain_berry_gain_multiplier" real DEFAULT 1 NOT NULL;--> statement-breakpoint
+ALTER TABLE "character_template" ADD COLUMN "captain_karma_multiplier" real DEFAULT 1 NOT NULL;--> statement-breakpoint
+ALTER TABLE "character_template" ADD COLUMN "captain_morale_multiplier" real DEFAULT 1 NOT NULL;
