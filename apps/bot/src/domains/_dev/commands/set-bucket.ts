7a8c032c1286fda8281b5c285ed56e23a1db361a
Jhizzaiolo <hakim.madani06@gmail.com>
2026-05-27 20:19:31 +0200

    added success

--- apps/bot/src/domains/_dev/commands/set-bucket.ts
+++ apps/bot/src/domains/_dev/commands/set-bucket.ts
@@ -15,6 +15,6 @@ export const setBucketCommand: Command = {
     }
     const oldBucket = player.lastProcessedBucketId;
     await playerRepository.setLastProcessedBucketId(player.id, bucketId);
-    await message.reply({ embeds: [buildOpEmbed().setDescription(`${player.name} : bucket ${oldBucket} → ${bucketId}`)] });
+    await message.reply({ embeds: [buildOpEmbed('success').setDescription(`${player.name} : bucket ${oldBucket} → ${bucketId}`)] });
   },
 };
