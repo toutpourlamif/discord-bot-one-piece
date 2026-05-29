1e6750d948100c458ed5d44a73f24abd41e0bb31
Hakim Madani <107553328+Jhizzaiolo@users.noreply.github.com>
2026-05-29 10:49:17 +0200

    308 feature vue de composition et commande boarding (#359)
    * added boarding view
    
    * fixed embed name
    
    * added boarding to index
    
    * changes
    
    * added requested changes
    
    * added func

--- apps/bot/src/domains/crew/constants.ts
+++ apps/bot/src/domains/crew/constants.ts
@@ -4,3 +4,7 @@ export const MIN_CREW_NAME_LENGTH = 3;
 
 export const CREW_BUTTON_NAME = 'crew';
 export const SET_CAPTAIN_BUTTON_NAME = 'setcaptain';
+
+export const EMBARK_BUTTON_NAME = 'embark';
+export const DISEMBARK_BUTTON_NAME = 'disembark';
+export const BOARDING_BUTTON_NAME = 'boarding';
