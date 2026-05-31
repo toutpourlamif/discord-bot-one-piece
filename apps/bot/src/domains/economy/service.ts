8059740219e05ffddd9b92f9af64d98286d93475
Jhizzaiolo <hakim.madani06@gmail.com>
2026-05-31 11:44:17 +0200

    added requested changes

--- apps/bot/src/domains/economy/service.ts
+++ apps/bot/src/domains/economy/service.ts
@@ -1,5 +1,7 @@
 import * as playerRepository from '../economy/repository.js';
 
+import { computeBerryReward } from './utils/compute-berry-reward.js';
+
 // TODO: renommer la fonction quand besoin métier
 export async function buy(playerId: number, amount: bigint): Promise<bigint> {
   return playerRepository.debitBerry(playerId, amount);
@@ -8,4 +10,24 @@ export async function buy(playerId: number, amount: bigint): Promise<bigint> {
 // TODO: pareil qu'en haut, renommer la fonction quand besoin métier
 export async function sell(playerId: number, amount: bigint): Promise<bigint> {
   return playerRepository.creditBerry(playerId, amount);
+}
+
+type CreditBerryOptions = {
+  considerCaptainBoosts?: boolean;
+};
+
+type CreditBerryParams = {
+  playerId: number;
+  amount: bigint;
+  options?: CreditBerryOptions;
+};
+
+export async function creditBerry({ playerId, amount, options }: CreditBerryParams): Promise<bigint> {
+  let finalAmount = amount;
+
+  if (options?.considerCaptainBoosts) {
+    finalAmount = await computeBerryReward(playerId, amount);
+  }
+
+  return playerRepository.creditBerry(playerId, finalAmount);
 }
