e14873c11a660a4e216f4551c75e4dbfe2a81908
Jhizzaiolo <hakim.madani06@gmail.com>
2026-06-01 18:01:35 +0200

    changed order

--- apps/bot/src/domains/_dev/commands/buckets.ts
+++ apps/bot/src/domains/_dev/commands/buckets.ts
@@ -1,20 +1,12 @@
 import type { Command } from '../../../discord/types.js';
 import { buildOpEmbed } from '../../../discord/utils/build-op-embed.js';
-import { getBucketIdFromDate, getEndDateOfBucket, getLatestProcessableBucket, getStartDateOfBucket } from '../../event/engine/bucket.js';
+import { getEndDateOfBucket, getLatestProcessableBucket, getNowBucketId, getStartDateOfBucket } from '../../event/engine/bucket.js';
 import { formatBucketTime } from '../../event/utils/format-bucket-time.js';
-
-function formatBucketLine(bucketId: number): string {
-  const startDate = getStartDateOfBucket(bucketId);
-  const endDate = getEndDateOfBucket(bucketId);
-  //TODO: ajouter l'util wrapInBackticks quand dispo autour du bucketId a la places des etoiles
-  return `**${bucketId}**, \n${formatBucketTime(startDate)} - ${formatBucketTime(endDate)}`;
-}
 
 export const bucketsCommand: Command = {
   name: 'buckets',
   async handler({ message }) {
-    // TODO: remplacer par getNowBucketId quand la PR dédiée sera mergée.
-    const nowBucketId = getBucketIdFromDate(new Date());
+    const nowBucketId = getNowBucketId();
     const latestProcessableBucketId = getLatestProcessableBucket();
 
     const embed = buildOpEmbed('info')
@@ -24,3 +16,10 @@ export const bucketsCommand: Command = {
     await message.reply({ embeds: [embed] });
   },
 };
+
+function formatBucketLine(bucketId: number): string {
+  const startDate = getStartDateOfBucket(bucketId);
+  const endDate = getEndDateOfBucket(bucketId);
+  //TODO: ajouter l'util wrapInBackticks quand dispo autour du bucketId a la places des etoiles
+  return `**${bucketId}**, \n${formatBucketTime(startDate)} - ${formatBucketTime(endDate)}`;
+}
