a776ad61fb898dfce078013737fcada79733e3e9
402rayan <rayan.desplanque-hamadache@lacatholille.fr>
2026-06-04 20:11:25 +0200

    refactor(event): utilise lodash sample pour le gif de conséquence

--- apps/bot/src/discord/utils/build-consequence-embed.ts
+++ apps/bot/src/discord/utils/build-consequence-embed.ts
@@ -1,16 +1,13 @@
 import type { EmbedBuilder } from 'discord.js';
+import sample from 'lodash/sample.js';
 
 import { buildAssetUrl } from '../../shared/build-asset-url.js';
 
 import { buildOpEmbed } from './build-op-embed.js';
 
 export function buildConsequenceEmbed(): EmbedBuilder {
-  return buildOpEmbed('default').setImage(pickRandomGif()).setDescription('Cette décision aura des conséquences...');
+  return buildOpEmbed('default').setImage(sample(CONSEQUENCE_GIFS)!).setDescription('Cette décision aura des conséquences...');
 }
 
 // TODO: ajouter d'autres papillons quand on en aura
 const CONSEQUENCE_GIFS: Array<string> = [buildAssetUrl('miscellaneous/butterfly-imu.webp')];
-
-function pickRandomGif(): string {
-  return CONSEQUENCE_GIFS[Math.floor(Math.random() * CONSEQUENCE_GIFS.length)]!;
-}
