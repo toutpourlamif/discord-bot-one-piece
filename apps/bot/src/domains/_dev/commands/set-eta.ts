cb02e70b4f92bae9e3838c07415e110037474593
Jhizzaiolo <hakim.madani06@gmail.com>
2026-06-07 13:07:15 +0200

    added seteta command and function in repository

---
+++ apps/bot/src/domains/_dev/commands/set-eta.ts
@@ -0,0 +1,27 @@
+import { ValidationError } from '../../../discord/errors.js';
+import type { Command } from '../../../discord/types.js';
+import { buildOpEmbed } from '../../../discord/utils/build-op-embed.js';
+import { parseIntegerArg } from '../../../discord/utils/parse-integer-arg.js';
+import { resolveTargetPlayer } from '../../player/index.js';
+import * as playerRepository from '../../player/repository.js';
+
+export const setEtaCommand: Command = {
+  name: ['set-eta', 'seteta'],
+  async handler(ctx) {
+    const { targetPlayer, rest } = await resolveTargetPlayer(ctx);
+    const [bucketNumber] = rest;
+    const { travelTargetZone } = targetPlayer;
+
+    const parsedBucket = parseIntegerArg(bucketNumber);
+
+    if (!travelTargetZone) {
+      throw new ValidationError(`Usage: !seteta <@user> <bucket(integer)>`);
+    }
+
+    await playerRepository.setTravelEtaBucket(targetPlayer.id, parsedBucket);
+
+    const embed = buildOpEmbed('info').setTitle('Eta').setDescription(`Eta déplacé au bucket ${parsedBucket}`);
+
+    await ctx.message.reply({ embeds: [embed] });
+  },
+};
