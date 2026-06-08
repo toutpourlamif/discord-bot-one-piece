cd79c7ac64490e87415c4f1fc15a10f968c35396
Jhizzaiolo <hakim.madani06@gmail.com>
2026-06-08 17:08:19 +0200

    added requested changes

--- apps/bot/src/domains/_dev/commands/set-eta.ts
+++ apps/bot/src/domains/_dev/commands/set-eta.ts
@@ -2,7 +2,6 @@ import { ValidationError } from '../../../discord/errors.js';
 import type { Command } from '../../../discord/types.js';
 import { buildOpEmbed } from '../../../discord/utils/build-op-embed.js';
 import { parseIntegerArg } from '../../../discord/utils/parse-integer-arg.js';
-import { getNowBucketId } from '../../event/engine/bucket.js';
 import { resolveTargetPlayer } from '../../player/index.js';
 import * as playerRepository from '../../player/repository.js';
 
@@ -10,23 +9,20 @@ export const setEtaCommand: Command = {
   name: ['set-eta', 'seteta'],
   async handler(ctx) {
     const { targetPlayer, rest } = await resolveTargetPlayer(ctx);
-    const [bucketNumber] = rest;
     const { travelTargetZone } = targetPlayer;
-    const bucketNow = getNowBucketId();
-
-    const parsedBucket = parseIntegerArg(bucketNumber);
+    const parsedBucket = parseIntegerArg(rest[0]);
 
     if (!travelTargetZone) {
       throw new ValidationError(`Usage: !seteta <@user> <bucket(integer)> \n\n Il faut un voyage en cours!`);
     }
 
-    if (parsedBucket < bucketNow) {
+    if (parsedBucket < parsedBucket) {
       throw new ValidationError(`Tu ne peux pas arriver dans le passé \n(la machine à remonter le temps n'existe pas encore.)`);
     }
 
     await playerRepository.setTravelEtaBucket(targetPlayer.id, parsedBucket);
 
-    const embed = buildOpEmbed('info').setTitle('Eta').setDescription(`Eta déplacé au bucket ${parsedBucket}`);
+    const embed = buildOpEmbed('success').setTitle('Eta').setDescription(`Eta déplacé au bucket ${parsedBucket}`);
 
     await ctx.message.reply({ embeds: [embed] });
   },
