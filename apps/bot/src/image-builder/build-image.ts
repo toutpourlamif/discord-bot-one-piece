6e44719b93bb76aa686f6c3f44d30b7fa253b32f
402rayan <rayan.desplanque-hamadache@lacatholille.fr>
2026-06-12 16:27:06 +0200

    refactor(image-builder): extrait rasterize-svg (RETINA_SCALE unique) et expose le chemin de la font

--- apps/bot/src/image-builder/build-image.ts
+++ apps/bot/src/image-builder/build-image.ts
@@ -1,10 +1,8 @@
-import { Resvg } from '@resvg/resvg-js';
 import type { ReactNode } from 'react';
 import satori from 'satori';
 
 import { fonts } from './fonts.js';
-
-const RETINA_SCALE = 2;
+import { rasterizeSvg } from './rasterize-svg.js';
 
 export type BuildImageOptions = {
   width: number;
@@ -13,7 +11,5 @@ export type BuildImageOptions = {
 
 export async function buildImage(element: ReactNode, options: BuildImageOptions): Promise<Buffer> {
   const svg = await satori(element, { width: options.width, height: options.height, fonts });
-
-  const png = new Resvg(svg, { fitTo: { mode: 'width', value: options.width * RETINA_SCALE } }).render().asPng();
-  return Buffer.from(png);
+  return rasterizeSvg(svg, { width: options.width });
 }
