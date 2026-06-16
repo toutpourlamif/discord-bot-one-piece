863f17a6b7c45822e1232ffbe5e1936b99af9600
Jhizzaiolo <hakim.madani06@gmail.com>
2026-06-16 20:00:32 +0200

    added requested changes

--- apps/bot/src/discord/utils/format-command.ts
+++ apps/bot/src/discord/utils/format-command.ts
@@ -2,7 +2,7 @@ import { wrapInBackticks } from './index.js';
 
 export function formatCommand(prefix: string, commandName: string, options?: FormatCommandOptions): string {
   const command = `${prefix}${commandName}`;
-  return options?.hasBackticks === false ? command : wrapInBackticks(command);
+  return options?.hasBackticks ? wrapInBackticks(command) : command;
 }
 
 type FormatCommandOptions = {
