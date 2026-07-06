d276a9b1d7b36be8c46291e19446fbf5654545b3
402rayan <rayan.desplanque-hamadache@lacatholille.fr>
2026-07-06 15:52:26 +0200

    add data

---
+++ packages/db/src/domains/character/character_template/data/yotsuba-island.ts
@@ -0,0 +1,14 @@
+import type { CharacterTemplateSeed } from './types.js';
+
+export const YOTSUBA_ISLAND_DATA: Array<CharacterTemplateSeed> = [
+  {
+    name: 'Alvida',
+    hp: 11,
+    combat: 12,
+    race: 'HUMAN',
+    rarity: 'D',
+    imageUrl: 'characters/yotsuba-island/alvida/info.webp',
+    // TODO: trouver une vraie desc
+    description: 'CERTIFIÉE GROSSE VACHE (après elle devient skinny tkt)',
+  },
+];
