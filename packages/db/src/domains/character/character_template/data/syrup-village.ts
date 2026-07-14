c37f6b23caf09a4579f225de6ccfc0e93c38b27a
Jhizzaiolo <hakim.madani06@gmail.com>
2026-07-14 14:39:26 +0200

    added character kuro

---
+++ packages/db/src/domains/character/character_template/data/syrup-village.ts
@@ -0,0 +1,14 @@
+import type { CharacterTemplateSeed } from './types.js';
+
+export const SYRUP_VILLAGE_DATA: Array<CharacterTemplateSeed> = [
+  {
+    name: 'Kuro',
+    hp: 10,
+    combat: 10,
+    race: 'HUMAN',
+    types: ['NORMAL', 'FIGHTING'],
+    rarity: 'D',
+    imageUrl: 'characters/syrup-village/kuro/info.webp',
+    description: 'Capitaine de l`équipage des chats noirs, Kuro est un sociopathe sadique munis de griffes en métal',
+  },
+];
