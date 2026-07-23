606c768313c2c6d6631e0951114ef946f3a06472
Jhizzaiolo <hakim.madani06@gmail.com>
2026-07-23 16:49:45 +0200

    modified desc

--- packages/db/src/domains/character/character_template/data/dawn-island.ts
+++ packages/db/src/domains/character/character_template/data/dawn-island.ts
@@ -9,6 +9,6 @@ export const DAWN_ISLAND_DATA: Array<CharacterTemplateSeed> = [
     race: 'HUMAN',
     rarity: 'D',
     imageUrl: 'characters/dawn-island/woop-slap/info.webp',
-    description: 'Maire aimé du village de Fuschia, c`est un vieux de la vieille qui n`a pas peur de critiquer les pirates',
+    description: `Maire aimé du village de Fuschia, c'est un vieux de la vieille qui n'a pas peur de critiquer les pirates`,
   },
 ];
