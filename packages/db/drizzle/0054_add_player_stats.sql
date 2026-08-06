54957edd68cea6b142bb8d18b1690479eeb0199b
402rayan <rayan.desplanque-hamadache@lacatholille.fr>
2026-08-06 15:09:20 +0200

    add schema

---
+++ packages/db/drizzle/0054_add_player_stats.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "player" ADD COLUMN "intelligence" integer DEFAULT 0 NOT NULL;--> statement-breakpoint
+ALTER TABLE "player" ADD COLUMN "charisme" integer DEFAULT 0 NOT NULL;--> statement-breakpoint
+ALTER TABLE "player" ADD COLUMN "gentillesse" integer DEFAULT 0 NOT NULL;--> statement-breakpoint
+ALTER TABLE "player" ADD COLUMN "audace" integer DEFAULT 0 NOT NULL;
