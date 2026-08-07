fad1c64aeeba9e8862cdd0acce29ef13f0b82d37
402rayan <79017298+402rayan@users.noreply.github.com>
2026-08-07 11:37:30 +0200

    Feat/yotsuba v1 (#445)
    * remove shitmotsuki island
    
    * ADD SCRIPTS

---
+++ packages/db/drizzle/0054_remove_shimotsuki_village.sql
@@ -0,0 +1,6 @@
+ALTER TABLE "player" ALTER COLUMN "current_sub_zone" SET DATA TYPE text;--> statement-breakpoint
+ALTER TABLE "player" ALTER COLUMN "current_sub_zone" SET DEFAULT 'goa_kingdom'::text;--> statement-breakpoint
+DROP TYPE "public"."sub_zone";--> statement-breakpoint
+CREATE TYPE "public"."sub_zone" AS ENUM('satsuruzo_port', 'goa_kingdom', 'foosha_village', 'goat_coast', 'shells_town', 'mirrorball_coast', 'nagagutsu_port', 'orange_town', 'rare_animal_coast', 'kumate_coast', 'sixis_coast', 'tequila_wolf_coast', 'syrup_village', 'baratie_deck', 'arlong_park', 'cocoyasi_village', 'gosa_town', 'branch_16', 'cozia_port', 'frauce_port', 'oykot_port', 'loguetown_square', 'reverse_mountain_twin_cape', 'whisky_peak_town', 'little_garden_jungle', 'drum_village', 'alabasta_nanohana', 'wano_flower_capital');--> statement-breakpoint
+ALTER TABLE "player" ALTER COLUMN "current_sub_zone" SET DEFAULT 'goa_kingdom'::"public"."sub_zone";--> statement-breakpoint
+ALTER TABLE "player" ALTER COLUMN "current_sub_zone" SET DATA TYPE "public"."sub_zone" USING "current_sub_zone"::"public"."sub_zone";
